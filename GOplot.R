d58fda7bed83799014c8d0f142f84f480c2d7259
Ishak Yusuf <142504615+isky20@users.noreply.github.com>
2024-06-24 14:43:38 +0200

    Update GOplot.R

--- GOplot.R
+++ GOplot.R
@@ -1,16 +1,20 @@
 library(GOplot)
 library(tidyr)
 
+# Initialize a list to store terms
 term <- list()
-#file should have col called genes, term, logFC
+
+# Loop through all CSV files in the directory
 for (i in list.files(pattern = ".csv")) {
-  print(i)
-  df1 <- read.csv(i)[,c("genes","term")]
-  term[[i]] <- df1$term
+  print(i)  # Print the name of the file being processed
+  df1 <- read.csv(i)[,c("genes","term")]  # Read the CSV file and select 'genes' and 'term' columns
+  term[[i]] <- df1$term  # Store the terms in the list
 }
 
+# Get unique terms from all files
 terms <- unique(unlist(term))
 
+# Define a vector of colors
 colors <- c("coral", "blue", "mediumvioletred", "sandybrown",
             "salmon", "lavender", "paleturquoise", "orchid", "tan",
             "steelblue", "peru", "thistle", "slateblue",
@@ -25,32 +29,35 @@ colors <- c("coral", "blue", "mediumvioletred", "sandybrown",
             "lightyellow", "linen", "palegreen", "hotpink", "peachpuff",
             "seashell", "lavenderblush", "mintcream", "honeydew", "ivory")[1:length(terms)]
 
-dcolor <- data.frame(terms=terms,color= colors)
+# Create a dataframe with terms and their corresponding colors
+dcolor <- data.frame(terms = terms, color = colors)
 
+# Specify the file ID (without the .csv extension)
+ST <- "TTR"
 
+# Read the specified CSV file
+df <- read.csv(paste(ST, "csv", sep = "."))  # File should have columns 'genes', 'terms', and 'logFC'
 
-ST <- "TTR"   #Enter file ID (file ID.csv)
-df <- read.csv(paste(ST,"csv", sep = ".")) #file has three columns genes, terms and logFC 
-  
-  
-# Extract unique genes and process terms
+# Extract unique genes and their logFC values
 dff <- unique(df[, c("genes", "logFC")])
+
+# Prepare data for chord plot
 chord <- chord_dat(data = df, genes = dff, process = unique(df$term))
-  
-  # Select terms for coloring
+
+# Select terms for coloring
 l <- head(colnames(chord), -1)
-indices <- match(dcolor$term, l, nomatch = 0)
+indices <- match(dcolor$terms, l, nomatch = 0)
 sub_df <- dcolor[indices != 0, ]
-sub_df <- sub_df[match(l, sub_df$term), ]
-  
-# Generate plot
-svg(paste(ST,"svg", sep = "."), width = 20, height = 20)
+sub_df <- sub_df[match(l, sub_df$terms), ]
+
+# Generate the chord plot
+svg(paste(ST, "svg", sep = "."), width = 20, height = 20)
 par(mar = c(1, 1, 1, 1))
 GOChord(chord, space = 0.02, gene.order = 'logFC',
-          gene.space = 0.25, gene.size = 6,
-          lfc.min = min(df$logFC), 
-          lfc.max = max(df$logFC),
-          lfc.col = "red", 
-          ribbon.col = c(sub_df$color))
+        gene.space = 0.25, gene.size = 6,
+        lfc.min = min(df$logFC), 
+        lfc.max = max(df$logFC),
+        lfc.col = "red", 
+        ribbon.col = c(sub_df$color))
 dev.off()
-  
+
